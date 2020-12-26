44bfda964ab790cf9a5b97979df29c20687f185b
Dinos <papakosk@csd.auth.gr>
2020-12-26 21:29:09 +0200

    Change writeString behavior to default (which creates the file too)

--- src/main/java/me/din0s/java10_11/FilesConvenience.java
+++ src/main/java/me/din0s/java10_11/FilesConvenience.java
@@ -3,14 +3,13 @@ package me.din0s.java10_11;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 
 public class FilesConvenience {
     public static void main(String[] args) throws IOException {
         var path = FileSystems.getDefault().getPath("file.txt");
 
         // writing - works even if the file doesn't exist!
-        Files.writeString(path, "hello world", StandardOpenOption.APPEND);
+        Files.writeString(path, "hello world");
 
         // reading
         var allLines = Files.readString(path);
